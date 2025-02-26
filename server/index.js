9007fc136104d39343d742ffdc7b3918636281b1
Thomas Chu <thomas.chu@edu.univ-fcomte.fr>
2025-02-26 19:18:58 +0100

    Project initialization

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Root endpoint
+app.get('/', (req, res) => {
+    res.json({ message: "Hello from the Node.js backend!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
