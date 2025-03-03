760a17fbd1d55ec06e263c0b014647ba4daddf2d
Thomas Chu <thomas.chu@edu.univ-fcomte.fr>
2025-03-03 21:39:48 +0100

    Configure api settings in the front

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  randomImageUrl: string | null = null;
+  cities = ['frankfurt', 'lindau', 'munster']; // TODO: This is the cities hardcoded in the server, should be fetched from the server
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadRandomImage();
+  }
+
+  loadRandomImage() {
+    const randomCity = this.cities[2]; // I'm hardchoosing the third city, Munster for now
+    const randomImage = `munster_000001_000019_leftImg8bit.png`; // First image of the dataset
+
+    this.randomImageUrl = `${this.apiUrl}/images/${randomCity}/${randomImage}`;
+  }
 }
