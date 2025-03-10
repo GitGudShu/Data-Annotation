6659640c14757ce2a7cc2406d0cdbc25d5853c48
Thomas Chu <thomas.chu@edu.univ-fcomte.fr>
2025-03-10 14:40:12 +0100

    Update image service

--- client/src/app/classes/image.ts
+++ client/src/app/classes/image.ts
@@ -5,15 +5,23 @@ export class Image {
     public id: string,
     public filePath: string,
     public city: string,
-    public width: number,
-    public height: number,
+    public width?: number,
+    public height?: number,
     public annotations?: Annotation[]
-  ) {
-    this.id = id;
-    this.filePath = filePath;
-    this.city = city;
-    this.width = width;
-    this.height = height;
-    this.annotations = annotations;
+  ) {}
+
+  static fromJSON(json: any): Image {
+    return new Image(
+      json.id,
+      json.filePath,
+      json.city,
+      json.width,
+      json.height,
+      json.annotations ? json.annotations.map((obj: any) => Annotation.fromJSON(obj)) : []
+    );
+  }
+
+  static fromMinimalJSON(json: any): Image {
+    return new Image(json.id, json.filePath, json.city);
   }
 }
--- client/src/app/services/image.service.ts
+++ client/src/app/services/image.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Image } from '../classes/image';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
+  private apiUrl = 'http://localhost:5000/api/image-annotations';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getImagesByCity(cityName: string): Observable<Image[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/${cityName}`).pipe(
+      map(response => response.map(data => Image.fromMinimalJSON(data)))
+    );
+  }
 }
