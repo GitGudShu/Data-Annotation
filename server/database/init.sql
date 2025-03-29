8300d9b2d7cc883ececb9db3bda6057fda4ef573
Thomas Chu <thomas.chu@edu.univ-fcomte.fr>
2025-03-29 09:40:44 +0100

    Database updated (default user creation + role automation)

--- server/database/init.sql
+++ server/database/init.sql
@@ -11,6 +11,34 @@ CREATE TABLE users (
     avatar VARCHAR(255) DEFAULT NULL,
     password VARCHAR(150) NOT NULL,
     role ENUM('annotator', 'admin') DEFAULT 'annotator',
-    createdAt DATETIME NOT NULL,
-	updatedAt DATETIME NOT NULL
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Admin: Jean Admin
+INSERT INTO users (nom, prenom, email, avatar, password, role)
+VALUES (
+  'admin', 
+  'Jean', 
+  'jean.admin@example.com', 
+  'http://localhost:5000/uploads/avatars/admin.jpeg', 
+  'qsd', 
+  'admin'
+);
+
+-- Annotator: Lewis Hamilton and Silly Cat
+INSERT INTO users (nom, prenom, email, avatar, password)
+VALUES (
+  'Hamilton', 
+  'Lewis', 
+  'lewis.hamilton@example.com', 
+  'http://localhost:5000/uploads/avatars/shu.jpg', 
+  'aze'
+),
+(
+  'Cat', 
+  'Silly', 
+  'silly.cat@example.com', 
+  'http://localhost:5000/uploads/avatars/shu.jpg', 
+  'aze'
+);
