ba143c3c306459fc288d2b3df28ca2ba86eecf3b
Bilel BOUHEDDA <115164152+BilelBOUHEDDA@users.noreply.github.com>
2025-03-31 16:13:06 +0200

    New table in database : Tickets

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE IF NOT EXISTS annotation_appBDD;
 USE annotation_appBDD;
+CREATE DATABASE IF NOT EXISTS annotation_appBDD;
 
+DROP TABLE IF EXISTS `tickets`;
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE users (
@@ -15,7 +16,30 @@ CREATE TABLE users (
     updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Admin: Jean Admin
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ticketName VARCHAR(50) DEFAULT NULL,
+    description TEXT,
+    sample VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    userId INT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DELIMITER //
+CREATE TRIGGER trg_tickets_after_insert
+AFTER INSERT ON tickets
+FOR EACH ROW
+BEGIN
+  UPDATE tickets
+  SET ticketName = CONCAT('ticket-', LPAD(NEW.id, 5, '0'))
+  WHERE id = NEW.id;
+END; //
+DELIMITER ;
+
+-- Insertion d'exemples d'utilisateurs
 INSERT INTO users (nom, prenom, email, avatar, password, role)
 VALUES (
   'admin', 
@@ -26,9 +50,9 @@ VALUES (
   'admin'
 );
 
--- Annotator: Lewis Hamilton and Silly Cat
 INSERT INTO users (nom, prenom, email, avatar, password)
-VALUES (
+VALUES 
+(
   'Hamilton', 
   'Lewis', 
   'lewis.hamilton@example.com', 
